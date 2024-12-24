921de45947e4cc7d9669543b912230045246bb6c
Fabian <fabianmendozaospina@outlook.com>
2024-12-24 00:08:28 -0600

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/drinks-recipes-finder/'
+  base: '/', //isProduction ? '/drinks-recipes-finder/' : '/',
 })
